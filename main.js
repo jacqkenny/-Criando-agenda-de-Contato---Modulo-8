7ae6a4576d57a2c1c46f17cbf8fd420cfb2e5b1e
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2023-05-10 22:10:30 -0300

    salvando alterações(index.html)

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const inputNomeContato = document.getElementById('nome-contato');
-    const inputNúmeroContato = document.getElementById('número-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
 
     let linha = '<tr>';
     linha += `<td>${inputNomeContato.Value}</td)`;
-    linha += `<td>${inputNúmeroContato.Value}</td)`;
+    linha += `<td>${inputNumeroContato.Value}</td)`;
     linha += '</tr>';
 
     linhas+= linha;
